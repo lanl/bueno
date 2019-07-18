f5045eaf523f08d32567f8ef7f4624e312b7309b
Samuel K. Gutierrez <samuel@lanl.gov>
2019-07-18 13:52:55 -0600

    Add metadata to image.

--- bueno/build/charliecloud.py
+++ bueno/build/charliecloud.py
@@ -13,7 +13,9 @@ The CharlieCloud container builder.
 from bueno.core import shell
 from bueno.core import utils
 from bueno.core import logger
+
 from bueno.build import builder
+from bueno.build import metadata
 
 import os
 
@@ -78,10 +80,10 @@ class impl(builder.Base):
 
     def _emit_build_spec(self):
         dockerf = os.path.join(self.config['spec'], self.spec_name)
-        logger.log('# Begin spec output')
+        logger.log('# Begin Spec Output')
         for line in shell.cat(dockerf):
             logger.log(line)
-        logger.log('# End spec output')
+        logger.log('# End Spec Output')
 
     def _get_path_to_storage(self):
         cmd = '{} -b {} -t {} --print-storage {}'.format(
@@ -95,15 +97,28 @@ class impl(builder.Base):
         # storage path.
         lst = list(filter(lambda x: 'building with' not in x, cmdo.split('\n')))
         if len(lst) < 1:
-            msg = 'Could not determine storage path from the following:\n{}'
+            msg = 'Could not determine storage ' \
+                  'path from the following output:\n{}'
             raise RuntimeError(msg.format(cmdo))
-        return lst[0]
+        # Hope for the best because of the rudimentary filtering used (i.e.,
+        # hope lst[0] is a valid path). If this ever becomes problematic,
+        # implement something nicer. Also, not a huge deal because the returned
+        # value will be used in later file operations (let them fail).
+        basep = lst[0]
+        # Now build up the entire image path. The convention appears to be:
+        # basep/img/tag:latest
+        specn = '{}:latest'.format(self.config['tag'])
+        return os.path.join(basep, 'img', specn)
 
     def _add_metadata(self):
-        logger.log('# Adding metadata to container')
+        logger.log('# Adding metadata to container...')
         spath = self._get_path_to_storage()
-        lfs = '# Looking for {} in {}'
-        logger.log(lfs.format(self.config['tag'], spath))
+        logger.log('# Looking for {}'.format(spath))
+        if not os.path.exists(spath):
+            msg = 'The following path does not exist: {}'.format(spath)
+            raise RuntimeError(msg)
+        logger.log('# Looks good. Adding metadata...')
+        metadata.add(spath)
 
     def _flatten(self):
         tcmd = '{} {} {}'.format(
@@ -112,10 +127,10 @@ class impl(builder.Base):
             self.config['output_path']
         )
 
-        logger.log('# Begin flatten output')
+        logger.log('# Begin Flatten Output')
         os.environ['CH_BUILDER'] = self.builder
         shell.run(tcmd, echo=True)
-        logger.log('# End flatten output')
+        logger.log('# End Flatten Output')
 
     def _build(self):
         bcmd = '{} -b {} -t {} {}'.format(
@@ -124,17 +139,13 @@ class impl(builder.Base):
             self.config['tag'],
             self.config['spec']
         )
-        logger.log('# Begin build output')
+        logger.log('# Begin Build Output')
         # Run the command specified by bcmd.
         shell.run(bcmd, echo=True)
-        logger.log('# End build output')
+        logger.log('# End Build Output')
 
     def start(self):
         self._check_env()
-
-        self._add_metadata()
-        exit()
-
         self._emit_builder_info()
         self._emit_build_spec()
         self._build()
--- bueno/build/metadata.py
+++ bueno/build/metadata.py
@@ -9,3 +9,28 @@
 '''
 Stores build metadata.
 '''
+
+from bueno.core import utils
+from bueno.core import logger
+
+import os
+
+
+def add(basep):
+    _MetaData(basep).add()
+
+
+class _MetaData:
+    def __init__(self, basep):
+        self.basep = basep
+        self.metad = os.path.join(basep, 'bueno')
+        self.buildo = 'build-output.txt'
+
+        self._mkdir()
+
+    def add(self):
+        logger.log('# Done {}'.format(utils.nows()))
+        logger.save(os.path.join(self.metad, self.buildo))
+
+    def _mkdir(self):
+        os.makedirs(self.metad, 0o755)
