b1f2d0c71e947301764f95d47c8cd46852896f11
Samuel K. Gutierrez <samuel@lanl.gov>
2019-07-26 16:59:47 -0600

    Improve shell-cmds test.

--- tests/container/shell-cmds/shell-cmds.py
+++ tests/container/shell-cmds/shell-cmds.py
@@ -12,17 +12,24 @@ Tests for container.run()
 
 from bueno.public import shell
 from bueno.public import logger
+from bueno.public import metadata
 from bueno.public import container
 
 
 def main(argv):
+    fname = 'afile.txt'
     logger.log('# Testing globbing...')
     container.run('ls b*')
 
     logger.log('# Testing redirection...')
-    container.run('echo "Some Text" | tee OUT.txt')
-    container.run('echo "More \'Text\'" >> OUT.txt')
-    shell.run('cat OUT.txt')
+    logger.log('# Adding text to {}:'.format(fname))
+    container.run('echo "Some Text" | tee {}'.format(fname))
+    container.run('echo "More \'Text\'" >> {}'.format(fname))
+
+    logger.log('# The contents of {} are:'.format(fname))
+    shell.run('cat {}'.format(fname))
 
     logger.log('# Testing quoting...')
     container.run('echo "Some \'Text\'"')
+
+    metadata.add_asset(metadata.FileAsset(fname))
