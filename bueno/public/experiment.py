b97cebe8a85ab109d34c3ad5dac27fdf1f8977e4
Samuel K. Gutierrez <samuel@lanl.gov>
2019-08-08 11:01:59 -0600

    Start refactor of CLI configuration into experiment.

--- bueno/public/experiment.py
+++ bueno/public/experiment.py
@@ -14,7 +14,10 @@ from bueno.core import metacls
 
 from bueno.public import utils
 
+from abc import ABC, abstractmethod
+import argparse
 import copy
+import os
 
 # TODO(skg) Add a convenience function that allows for the specification of a
 # list (in priority order) of executables. The first one found wins!
@@ -44,10 +47,11 @@ def generate(spec, *args):
         es = '{}.generate() expects a string specification.'.format(__name__)
         raise ValueError(es)
     argg = zip(* args)
+
     return [spec.format(*a) for a in argg]
 
 
-def readgs(gs, argp=None):
+def readgs(gs, argprsr=None):
     '''
     A convenience routine for reading generate specification files.
 
@@ -65,7 +69,7 @@ def readgs(gs, argp=None):
             # Interpret as special comment used to specify run-time arguments.
             if l.startswith('# -'):
                 # Add to argument list.
-                if argp is not None:
+                if argprsr is not None:
                     argv.extend(l.lstrip('# ').split())
             # Skip comments.
             elif l.startswith('#'):
@@ -75,13 +79,17 @@ def readgs(gs, argp=None):
                 gsstr += l
         # Parse arguments if provided an argument parser.
         gsargs = None
-        if argp is not None:
-            gsargs = parsedargs(argp, argv)
+        if argprsr is not None:
+            gsargs = parsedargs(argprsr, argv)
+
     return gsstr, gsargs
 
-def parsedargs(argp, argv):
+    # TODO(skg) Emit arguments provided and parsed from file.
+def parsedargs(argprsr, argv):
+    '''
+    '''
     # Make a deep copy of the provided argument parser.
-    auxap = copy.deepcopy(argp)
+    auxap = copy.deepcopy(argprsr)
     # Known args, remaining (not used) = auxap.parse_known_args(argv)
     kargs, _ = auxap.parse_known_args(argv)
     # Set defaults to None so we can detect setting of arguments.
@@ -118,7 +126,54 @@ class _TheExperiment(metaclass=metacls.Singleton):
                'For example:\n' \
                'from bueno.public import experiment\n' \
                'experiment.{0}(\'aname\')\n'
+
         if utils.emptystr(self.name):
             es = 'Experiment name is not set. '
             es += fixs.format('name')
             raise RuntimeError(es)
+
+
+class CLIConfiguration:
+    '''
+    Command-line interface configuration container and associated utilities.
+    '''
+    def __init__(self, desc, argv):
+        self.desc = desc
+        self.argv = argv
+        self.prog = os.path.basename(argv[0])
+
+        self.argprsr = None
+        self._args = None
+
+        self.argprsr = argparse.ArgumentParser(
+            prog=self.prog,
+            description=self.desc,
+            allow_abbrev=False
+        )
+        self.addargs()
+
+    @abstractmethod
+    def addargs(self):
+        pass
+
+    def argparser(self):
+        return self.argprsr
+
+    def parseargs(self):
+        self._args = self.argprsr.parse_args(self.argv[1:])
+
+    def args(self):
+        return self._args
+
+    # TODO(skg) cleanup
+    def update(self, confns):
+        confd = vars(confns)
+        argsd = vars(self._args)
+        pcmdargs = vars(parsedargs(self.argprsr, self.argv[1:]))
+
+        # Look at the arguments provided in the configuration (gs) file.
+        for k, v in argsd.items():
+            if confd[k] is not None:
+                argsd[k] = confd[k]
+            if pcmdargs[k] is not None:
+                argsd[k] = pcmdargs[k]
