522e048f03496752318ac844d718adc501f9a38c
JacobDickens <Lcjacobpd@gmail.com>
2021-03-22 14:08:46 -0600

    Build Flags
    + build-docs.sh now includes --help and --backup flags
      for retaining a copy of the original html directory

--- docs/build-docs.sh
+++ docs/build-docs.sh
@@ -25,20 +25,47 @@ verify_prereqs() {
     done
 }
 
+################################################################################
+backup_html() {
+    # Remove and replace existing html-bkp dir
+    echo "Backing up html to html-bkp"
+    rm -rf html-bkp
+    mv html html-bkp
+}
+
+################################################################################
 main() {
+    for i in "$1"
+    do
+        case "$1" in
+            -h|--help)
+                echo "options:"
+                echo "-h, --help    show brief help"
+                echo "-b, --backup  backup html directory"
+                echo ""
+                exit 0;
+                ;;
+            -b|--backup)
+                backup_html
+                shift
+                ;;
+        esac
+    done
+
     set -e
 
     verify_prereqs
     cddocs
 
-    echo "Building Documentation in $PWD"
-    git rm -rf html
+    echo "Building Documentation in $PWD";
+    git rm -rf --ignore-unmatch html
     make html
     mv build/html .
     git add html
     echo "All done!"
 }
 
-main
+
+main $*
 
 # vim: ts=4 sts=4 sw=4 expandtab
